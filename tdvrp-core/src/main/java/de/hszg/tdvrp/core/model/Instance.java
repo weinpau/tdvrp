f6a209fa00a9a1075a2133b84287930e6dec0903
Paul Weinhold <weinpau@web.de>
2014-11-25 13:39:35 +0100

    fixed typo

--- tdvrp-core/src/main/java/de/hszg/tdvrp/core/model/Instance.java
+++ tdvrp-core/src/main/java/de/hszg/tdvrp/core/model/Instance.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface Instance {
 
     /**
-     * Returns the name of the instance,
+     * Returns the name of the instance.
      *
      * @return name of instance
      */
