779adea06f7db37e18947016068a45337aafaa26
Paul Weinhold <weinpau@web.de>
2014-12-12 08:31:31 +0100

    added RandomSelection

---
+++ tdvrp-solver.ga/src/main/java/de/hszg/tdvrp/solver/ga/selection/RandomSelection.java
@@ -0,0 +1,34 @@
+package de.hszg.tdvrp.solver.ga.selection;
+
+import de.hszg.tdvrp.solver.ga.Chromosome;
+import de.hszg.tdvrp.solver.ga.Population;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ *
+ * @author weinpau
+ */
+public class RandomSelection implements Selection {
+
+    @Override
+    public Collection<Chromosome> select(Population population, int selectionSize) {
+
+        Collection<Chromosome> result = new ArrayList<>();
+        List<Chromosome> chromosomes = new ArrayList<>(population.getChromosomes());
+        Collections.shuffle(chromosomes);
+
+        while (result.size() < selectionSize) {
+            int size = result.size();
+            for (int i = 0; i < Math.max(selectionSize, chromosomes.size()) - size; i++) {
+                result.add(chromosomes.get(i));
+            }
+        }
+
+        return result;
+    }
+
+}
